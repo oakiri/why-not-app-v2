62d233fd4f243a4c9d0e1f6551bc7a352f34caf6
oakiri <81296352+oakiri@users.noreply.github.com>
2025-12-12 12:22:14 +0100

    Use App.js as navigation entry point

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
-import { StatusBar } from 'expo-status-bar';
-
-const WhyNotTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: '#0b0b0b',
-    primary: '#f9d648',
-    card: '#0b0b0b',
-    text: '#ffffff',
-    border: '#f9d648',
-    notification: '#f53b57',
-  },
-};
 
 export default function App() {
   return (
-    <NavigationContainer theme={WhyNotTheme}>
-      <StatusBar style="light" />
+    <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
   );
