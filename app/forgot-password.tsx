8a2ca67ece74a0bf4e152bf3aff1e04cf99a3fd0
oakiri <oakiri@gmail.com>
2025-12-15 18:05:13 +0100

    Fix forgot password navigation to /forgot-password

--- app/forgot-password.tsx
+++ app/forgot-password.tsx
@@ -1,5 +1,10 @@
+import React, { useEffect } from 'react';
 import ForgotPasswordScreen from '../src/screens/auth/ForgotPasswordScreen';
 
 export default function ForgotPassword() {
-  return <ForgotPasswordScreen />;
+useEffect(() => {
+console.log('[AUTH] Entered route: /forgot-password');
+}, []);
+
+return <ForgotPasswordScreen />;
 }
